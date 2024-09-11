1514facc018cfdb29abebbadc8617ac102f800a5
TarzanIllges <TarzanIllges@gmail.com>
2024-09-11 12:50:14 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,59 +1,40 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
-
-
-# Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    country = models.CharField(max_length=100, blank=True, null=True)
-    established_year = models.IntegerField(blank=True, null=True)
+    description = models.TextField()
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    ]
 
     def __str__(self):
         return self.name
 
 
-# Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
 class CarModel(models.Model):
-    SEDAN = "Sedan"
-    SUV = "SUV"
-    WAGON = "Wagon"
-    HATCHBACK = "Hatchback"
-    CONVERTIBLE = "Convertible"
-    COUPE = "Coupe"
-    TRUCK = "Truck"
-
-    CAR_TYPE_CHOICES = [
-        (SEDAN, "Sedan"),
-        (SUV, "SUV"),
-        (WAGON, "Wagon"),
-        (HATCHBACK, "Hatchback"),
-        (CONVERTIBLE, "Convertible"),
-        (COUPE, "Coupe"),
-        (TRUCK, "Truck"),
-    ]
-
-    car_make = models.ForeignKey(
-        CarMake, on_delete=models.CASCADE, related_name="models"
-    )  # Many-to-one relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default="Sedan")
-    year = models.IntegerField(default=2024)
-    color = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015),
+        ]
+    )
+# Other fields as needed
+
+
+def __str__(self):
+    return self.name
